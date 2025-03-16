a40a1cc032a50877bc269cc6eb41ff8607b0303e
Treety <158841555+TR33TY-CODES@users.noreply.github.com>
2025-03-16 10:39:50 +0100

    Improve codes and make them Standalone

--- server/announcement.lua
+++ server/announcement.lua
@@ -1,5 +1,3 @@
-local QBCore = exports['qb-core']:GetCoreObject()
-
 AddEventHandler('txAdmin:events:announcement', function(eventData)
     local author = eventData.author
     local message = eventData.message
@@ -7,7 +5,7 @@ AddEventHandler('txAdmin:events:announcement', function(eventData)
     TriggerClientEvent('vms_notifyv2:TopNotification', -1, {
         title = "ANNOUNCEMENT",
         description = "<span style='color:#985ebe'>" .. author .. "</span>: " .. message .. "</span>",
-        time = 10000,
+        time = 15000,
         color = "#985ebe",
         icon = "fa-solid fa-bullhorn",
         soundNative = {-1, 'Checkpoint_Hit', 'GTAO_FM_Events_Soundset', 0},
--- server/scheduledRestart.lua
+++ server/scheduledRestart.lua
@@ -1,83 +1,17 @@
-local QBCore = exports['qb-core']:GetCoreObject()
-
 AddEventHandler('txAdmin:events:scheduledRestart', function(eventData)
-    -- 10-minute warning
-    if eventData.secondsRemaining == 600 then
-        TriggerClientEvent('vms_notifyv2:TopNotification', -1, {
-            title = "SERVER RESTART",
-            description = "The server will restart in <span style='color:#ff5269'>~extra-bold~~pulse~10 minutes~s~~s~</span>.",
-            time = 10000,
-            color = "#985ebe",
-            icon = "fa-solid fa-power-off",
-            soundNative = {-1, 'Checkpoint_Hit', 'GTAO_FM_Events_Soundset', 0},
-        })
+    local remainingMinutes = math.floor(eventData.secondsRemaining / 60)
+    local remainingSeconds = eventData.secondsRemaining % 60
     
-    -- 5-minute warning
-    elseif eventData.secondsRemaining == 300 then
+    if eventData.secondsRemaining <= 600 and eventData.secondsRemaining % 60 == 0 then 
+        local timeText = remainingMinutes > 0 and (remainingMinutes .. " minute" .. (remainingMinutes > 1 and "s" or "")) or (remainingSeconds .. " seconds")
+        
         TriggerClientEvent('vms_notifyv2:TopNotification', -1, {
             title = "SERVER RESTART",
-            description = "The server will restart in <span style='color:#ff5269'>~extra-bold~~pulse~5 minutes~s~~s~</span>.",
+            description = "The server will restart in <span style='color:#ff5269'>~extra-bold~~pulse~" .. timeText .. "~s~~s~</span>." .. (remainingMinutes == 1 and " Please prepare to leave the server!" or ""),
             time = 10000,
             color = "#985ebe",
             icon = "fa-solid fa-power-off",
             soundNative = {-1, 'Checkpoint_Hit', 'GTAO_FM_Events_Soundset', 0},
         })
-    
-    -- 4-minute warning
-    elseif eventData.secondsRemaining == 240 then
-        TriggerClientEvent('vms_notifyv2:TopNotification', -1, {
-            title = "SERVER RESTART",
-            description = "The server will restart in <span style='color:#ff5269'>~extra-bold~~pulse~4 minutes~s~~s~</span>.",
-            time = 10000,
-            color = "#985ebe",
-            icon = "fa-solid fa-power-off",
-            soundNative = {-1, 'Checkpoint_Hit', 'GTAO_FM_Events_Soundset', 0},
-        })
-
-    -- 3-minute warning
-    elseif eventData.secondsRemaining == 180 then
-        TriggerClientEvent('vms_notifyv2:TopNotification', -1, {
-            title = "SERVER RESTART",
-            description = "The server will restart in <span style='color:#ff5269'>~extra-bold~~pulse~3 minutes~s~~s~</span>.",
-            time = 10000,
-            color = "#985ebe",
-            icon = "fa-solid fa-power-off",
-            soundNative = {-1, 'Checkpoint_Hit', 'GTAO_FM_Events_Soundset', 0},
-        })
-
-    -- 2-minute warning
-    elseif eventData.secondsRemaining == 120 then
-        TriggerClientEvent('vms_notifyv2:TopNotification', -1, {
-            title = "SERVER RESTART",
-            description = "The server will restart in <span style='color:#ff5269'>~extra-bold~~pulse~2 minutes~s~~s~</span>.",
-            time = 10000,
-            color = "#985ebe",
-            icon = "fa-solid fa-power-off",
-            soundNative = {-1, 'Checkpoint_Hit', 'GTAO_FM_Events_Soundset', 0},
-        })
-    
-    -- 1-minute warning
-    elseif eventData.secondsRemaining == 60 then
-        TriggerClientEvent('vms_notifyv2:TopNotification', -1, {
-            title = "SERVER RESTART",
-            description = "The server will restart in <span style='color:#ff5269'>~extra-bold~~pulse~1 minute~s~~s~</span>. Please prepare to leave the server!",
-            time = 10000,
-            color = "#985ebe",
-            icon = "fa-solid fa-power-off",
-            soundNative = {-1, 'Checkpoint_Hit', 'GTAO_FM_Events_Soundset', 0},
-        })
-    
-    -- 30-second warning
-    elseif eventData.secondsRemaining == 30 then
-        TriggerClientEvent('vms_notifyv2:TopNotification', -1, {
-            title = "SERVER RESTART",
-            description = "The server will restart in <span style='color:#ff5269'>~extra-bold~~pulse~30 seconds~s~~s~</span>. Please exit the server now!",
-            time = 10000,
-            color = "#985ebe",
-            icon = "fa-solid fa-power-off",
-            soundNative = {-1, 'Checkpoint_Hit', 'GTAO_FM_Events_Soundset', 0},
-        })
-
-        QBCore.Functions.SavePlayers()
     end
 end)
